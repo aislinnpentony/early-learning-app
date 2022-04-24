ca6cda8a0deae7d84da0316193d80e3bc28601e6
Conaill Gallagher <conaillgallagher@Conaills-MacBook-Air.local>
2022-04-24 21:26:22 +0100

    add math sheet

--- src/views/user/P1/MathSheet.vue
+++ src/views/user/P1/MathSheet.vue
@@ -4,7 +4,7 @@
         <p>Addition</p>
         <img class="math sheet" src="/p1/math/mathadd1.jpeg" /> <img class="math sheet" src="/p1/math/mathadd2.png" /> <img class="math sheet" src="/p1/math/mathadd3.png" />
         <p>Subtraction</p>
-        <img class="math sheet" src="/p1/math/mathsub1.png" /> <img class="math sheet" src="/p1/math/mathsub2.gif" /> <img class="math sheet" src="/p1/math/mathsub3.png" />
+               <img class="math sheet" src="/p1/math/mathsub1.png" /> <img class="math sheet" src="/p1/math/mathsub2.gif" /> <img class="math sheet" src="/p1/math/mathsub3.png" />
     </div>
 </template>
 
